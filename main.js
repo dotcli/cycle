e9e9f8fdaae724aa2f38afee75298e9e5100f90c
Changbai Li <lcb931023@gmail.com>
2018-08-03 01:10:47 +0800

    Rewrite the app with better separation of data & view

--- main.js
+++ main.js
@@ -2,13 +2,35 @@ const STATES = { WORKING: 'WORKING', RESTING: 'RESTING' }
 
 const FPS = 30
 
-// defaults
-const data = {
-  state: STATES.WORKING,
+// data that changes by user input
+const userData = new RData({
+  workMins: 0.25,
+  restMins: 0.5,
   startTime: Date.now(),
-  workMins: 25,
-  restMins: 5,
-}
+})
+
+// data computed from userData,
+// and only has to change when userData changes
+const lazyData = new RData({
+  workTime: userData.workMins * 60 * 1000,
+  restTime: userData.restMins * 60 * 1000,
+})
+lazyData.register('cycleTime', (lazyData.workTime + lazyData.restTime))
+
+userData.onChange('workMins', (val) => {
+  lazyData.workTime = val * 60 * 1000
+  lazyData.cycleTime = lazyData.workTime + lazyData.restTime
+})
+userData.onChange('restMins', (val) => {
+  lazyData.restTime = val * 60 * 1000
+  lazyData.cycleTime = lazyData.workTime + lazyData.restTime
+})
+
+const renderedData = new RData({
+  timeDisplay: '00:00',
+  state: '',
+  completionRate: 0,
+})
 
 /**
  * Pads the time display with extra zero, if needed.
@@ -31,19 +53,18 @@ const clockFace = {
     this.circumference = this.radius * 2 * Math.PI
     this.workClock.style.strokeDasharray = this.circumference
     this.restClock.style.strokeDasharray = this.circumference
-    this.switchState(STATES.WORKING)
   },
   switchState(newState) {
     if (newState === STATES.WORKING) {
-      this.workClock.style.opacity = 1;
-      this.restClock.style.opacity = 0;
+      this.workClock.style.opacity = 1
+      this.restClock.style.opacity = 0
       this.restClock.style.strokeDashoffset = this.circumference
     } else if (newState === STATES.RESTING) {
-      this.workClock.style.opacity = 0;
-      this.restClock.style.opacity = 1;
+      this.workClock.style.opacity = 0
+      this.restClock.style.opacity = 1
     }
   },
-  update(state, completionRate) {
+  updateRing(state, completionRate) {
     if (state === STATES.WORKING) {
       this.workClock.style.strokeDashoffset = completionRate * this.circumference
     } else if (state === STATES.RESTING) {
@@ -59,52 +80,63 @@ const view = {
   workAudio: document.querySelector('.workAudio'),
   restAudio: document.querySelector('.restAudio'),
 
-  update({state, startTime, workMins, restMins}) {
-    // calculate how much time's passed since timer's start
-    const timePassed = (Date.now() - startTime)
-    const workTime = workMins * 60 * 1000
-    const restTime = restMins * 60 * 1000
-    const cycleTime = workTime + restTime
-    const timeThisCycle = timePassed % cycleTime
-    // if the time since last cycle is less than work time,
-    // we are in working state.
-    // otherwise, resting state.
-
-    // TODO revise this part of logic
-    // to separate data modification from view update.
-    // view should only get info from data,
-    // not write to it.
-    let timeRemain;
-    let timeRemainRate;
-    if (timeThisCycle < workTime) {
-      data.state = STATES.WORKING
-      timeRemain = workTime - timeThisCycle
-      timeRemainRate = timeRemain / workTime
-    } else {
-      data.state = STATES.RESTING
-      timeRemain = cycleTime - timeThisCycle
-      timeRemainRate = timeRemain / restTime
-    }
-    const minutes = Math.floor(timeRemain / 60 / 1000)
-    const seconds = Math.floor((timeRemain / 1000) % 60)
-    const timeDisplay = `${leftPadTime(minutes)}:${leftPadTime(seconds)}`
-    this.timeDisplay.innerText = timeDisplay
-    this.stateDisplay.innerText = data.state
-    document.title = `${timeDisplay} - ${data.state}`
-
-    const completionRate = 1 - timeRemainRate
-    clockFace.update(data.state, completionRate)
-
-    // detect if state has changed on this update loop
-    if (state === data.state) return;
-    // switch out which clock to display
-    clockFace.switchState(data.state);
-    // play the corresponding sound
-    if (data.state === STATES.WORKING) this.workAudio.play();
-    else if (data.state === STATES.RESTING) this.restAudio.play();
-  }
+  playStateSwitchSound(newState) {
+    if (newState === STATES.WORKING) this.workAudio.play()
+    else if (newState === STATES.RESTING) this.restAudio.play()
+  },
+  updateTimeDisplay(display) {
+    this.timeDisplay.innerText = display
+  },
+  updateStateDisplay(text) {
+    this.stateDisplay.innerText = text
+  },
+  updateTitle(time, state) {
+    document.title = `${time} - ${state}`
+  },
 }
 
-clockFace.initialize();
+renderedData.onChange('timeDisplay', (val) => {
+  view.updateTimeDisplay(val)
+  view.updateTitle(val, renderedData.state)
+})
+renderedData.onChange('state', (val) => {
+  view.updateStateDisplay(val)
+  view.updateTitle(renderedData.timeDisplay, val)
+  view.playStateSwitchSound(val)
 
-setInterval(() => {view.update(data)}, FPS)
+  clockFace.switchState(val)
+  clockFace.updateRing(val, renderedData.completionRate)
+})
+renderedData.onChange('completionRate', (val) => {
+  clockFace.updateRing(renderedData.state, val)
+})
+
+
+function loop() {
+  // calculate how much time's passed since timer's start
+  const timePassed = (Date.now() - userData.startTime)
+  const timeThisCycle = timePassed % lazyData.cycleTime
+  // if the time since last cycle is less than work time,
+  // we are in working state.
+  // otherwise, resting state.
+  let timeRemain
+  let timeRemainRate
+  if (timeThisCycle < lazyData.workTime) {
+    renderedData.state = STATES.WORKING
+    timeRemain = lazyData.workTime - timeThisCycle
+    timeRemainRate = timeRemain / lazyData.workTime
+  } else {
+    renderedData.state = STATES.RESTING
+    timeRemain = lazyData.cycleTime - timeThisCycle
+    timeRemainRate = timeRemain / lazyData.restTime
+  }
+  renderedData.completionRate = 1 - timeRemainRate
+  
+  const minutes = Math.floor(timeRemain / 60 / 1000)
+  const seconds = Math.floor((timeRemain / 1000) % 60)
+  renderedData.timeDisplay = `${leftPadTime(minutes)}:${leftPadTime(seconds)}`
+}
+
+clockFace.initialize()
+
+setInterval(loop, 1000 / FPS)
